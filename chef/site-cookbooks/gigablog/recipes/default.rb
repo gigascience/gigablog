32716a6636cbd87bc6af29dda825371c338f68ce
pli888 <peter@gigasciencejournal.com>
2016-11-10 11:57:13 +0800

    Add gigablog cookbook

---
+++ chef/site-cookbooks/gigablog/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: gigablog
+# Recipe:: default
+#
+# Copyright 2016, GigaScience
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'wordpress'
+
+['vim', 'tree'].each do |pkg|
+    package pkg
+end
+
+# iptables not required for default development environment
+service 'iptables' do
+    action [:disable, :stop]
+end
