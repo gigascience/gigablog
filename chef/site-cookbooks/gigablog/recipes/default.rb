4304b25434e01b7edd04ca23fdc5edc024eadfe4
pli888 <peter@gigasciencejournal.com>
2016-12-07 16:40:17 +0800

    Configure GigaBlog site to use gigablog theme

--- chef/site-cookbooks/gigablog/recipes/default.rb
+++ chef/site-cookbooks/gigablog/recipes/default.rb
@@ -29,12 +29,21 @@ bash 'Install GigaBlog' do
   	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     sudo mv wp-cli.phar /usr/local/bin/wp
-    sudo /usr/local/bin/wp core install --path=/var/www/wordpress --url=localhost:9170 --title=GigaBlog --admin_user=peter --admin_email=peter@gigasciencejournal.com --admin_password=gigadb
+    sudo /usr/local/bin/wp core install --path=/var/www/wordpress --url=localhost:9170 --title=GigaBlog --admin_user=peter --admin_email=peter@gigasciencejournal.com --admin_password=gigadb --skip-email
     # Install WP WXR import plugin
     sudo /usr/local/bin/wp plugin install wordpress-importer --path=/var/www/wordpress
     sudo /usr/local/bin/wp plugin activate wordpress-importer --path=/var/www/wordpress
     sudo yum -y install php-xml
     # Import blog content
     sudo /usr/local/bin/wp import --path=/var/www/wordpress /vagrant/wxr/test.gigablog.wordpress.xml --authors=create
+    # Clean up
+    sudo /usr/local/bin/wp post delete --path=/var/www/wordpress 1
+    # Configure site
+    sudo /usr/local/bin/wp option update blogdescription 'Data driven blogging from the GigaScience Editors' --path=/var/www/wordpress
+    # Add link to gigablog theme
+    sudo ln -s /vagrant/theme/gigablog /var/www/wordpress/wp-content/themes/gigablog
+    # Activate gigablog theme
+    sudo /usr/local/bin/wp theme --path=/var/www/wordpress activate gigablog
+
     EOH
 end
