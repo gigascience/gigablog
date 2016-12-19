8a430d42a6b8d95dd1a2617feace49f878b4ec77
pli888 <peter@gigasciencejournal.com>
2016-12-19 15:05:11 +0800

    Add new attributes for users
    The attributes configure the users for GigaBlog.

--- chef/site-cookbooks/gigablog/recipes/default.rb
+++ chef/site-cookbooks/gigablog/recipes/default.rb
@@ -28,6 +28,32 @@ vagrant_dir = node[:gigablog][:root_dir]
 wxr_file = node[:gigablog][:wxr_file]
 wxr_path = "#{vagrant_dir}/wxr/#{wxr_file}"
 
+admin = node[:gigablog][:admin]
+admin_name = node[:gigablog][:admin_name]
+admin_email = node[:gigablog][:admin_email]
+admin_password = node[:gigablog][:admin_password]
+admin_role = node[:gigablog][:admin_role]
+user1 = node[:gigablog][:user1]
+user1_name = node[:gigablog][:user1_name]
+user1_email = node[:gigablog][:user1_email]
+user1_password = node[:gigablog][:user1_password]
+user1_role = node[:gigablog][:user1_role]
+user2 = node[:gigablog][:user2]
+user2_name =  node[:gigablog][:user2_name]
+user2_email = node[:gigablog][:user2_email]
+user2_password = node[:gigablog][:user1_password]
+user2_role = node[:gigablog][:user2_role]
+user3 = node[:gigablog][:user3]
+user3_name = node[:gigablog][:user3_name]
+user3_email = node[:gigablog][:user3_email]
+user3_password = node[:gigablog][:user3_password]
+user4_role = node[:gigablog][:user4_role]
+user4 = node[:gigablog][:user4]
+user4_name = node[:gigablog][:user4_name]
+user4_email = node[:gigablog][:user4_email]
+user4_password = node[:gigablog][:user4_password]
+user5_role = node[:gigablog][:user5_role]
+
 bash 'Install GigaBlog' do
   cwd '/tmp'
   code <<-EOH
@@ -44,10 +70,11 @@ bash 'Install GigaBlog' do
     # Enable image cropping
     sudo yum -y install gd gd-devel php-gd
     # Create users
-	sudo /usr/local/bin/wp user create --path=#{wordpress_dir} scott peter@gigasciencejournal.com --role=author
-	sudo /usr/local/bin/wp user create --path=#{wordpress_dir} nicole peter@gigasciencejournal.com --role=author
-	sudo /usr/local/bin/wp user create --path=#{wordpress_dir} hans peter@gigasciencejournal.com --role=author
-	sudo /usr/local/bin/wp user create --path=#{wordpress_dir} laurie peter@gigasciencejournal.com --role=author
+    sudo /usr/local/bin/wp user create --path=#{wordpress_dir} #{admin} #{admin_email} --role=#{admin_role}
+	sudo /usr/local/bin/wp user create --path=#{wordpress_dir} #{user1} #{user1_email} --role=#{user1_role}
+	sudo /usr/local/bin/wp user create --path=#{wordpress_dir} #{user2} #{user2_email} --role=#{user2_role}
+	sudo /usr/local/bin/wp user create --path=#{wordpress_dir} #{user3} #{user3_email} --role=#{user3_role}
+	sudo /usr/local/bin/wp user create --path=#{wordpress_dir} #{user4} #{user4_email} --role=#{user4_role}
     # Import blog content
     sudo /usr/local/bin/wp import --path=#{wordpress_dir} #{wxr_path} --authors=create
     # Clean up
