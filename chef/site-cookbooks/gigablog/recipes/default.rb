a308339b94cf7900a6300dcbc512231215b30902
pli888 <peter@gigasciencejournal.com>
2016-12-23 07:52:20 +0800

    Remove second admin user creation
    The admin user was being created twice.

--- chef/site-cookbooks/gigablog/recipes/default.rb
+++ chef/site-cookbooks/gigablog/recipes/default.rb
@@ -48,7 +48,6 @@ bash 'Install GigaBlog' do
     # Enable image cropping
     sudo yum -y install gd gd-devel php-gd
     # Create users
-    sudo /usr/local/bin/wp user create --path=#{wordpress_dir} #{node[:gigablog][:admin]} #{node[:gigablog][:admin_email]} --role=#{node[:gigablog][:admin_role]}
 	sudo /usr/local/bin/wp user create --path=#{wordpress_dir} #{node[:gigablog][:user1]} #{node[:gigablog][:user1_email]} --role=#{node[:gigablog][:user1_role]} --user_pass=#{node[:gigablog][:user1_password]} --display_name=#{node[:gigablog][:user1_name]}
 	sudo /usr/local/bin/wp user create --path=#{wordpress_dir} #{node[:gigablog][:user2]} #{node[:gigablog][:user2_email]} --role=#{node[:gigablog][:user2_role]} --user_pass=#{node[:gigablog][:user2_password]} --display_name=#{node[:gigablog][:user2_name]}
 	sudo /usr/local/bin/wp user create --path=#{wordpress_dir} #{node[:gigablog][:user3]} #{node[:gigablog][:user3_email]} --role=#{node[:gigablog][:user3_role]} --user_pass=#{node[:gigablog][:user3_password]} --display_name=#{node[:gigablog][:user3_name]}
