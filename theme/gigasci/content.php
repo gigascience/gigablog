5a4d5ecc70df4c21c3c18e2a6ba94f9d1c923385
pli888 <peter@gigasciencejournal.com>
2017-02-17 16:24:54 +0800

    Only show excerpts of blog posts on home page

--- theme/gigasci/content.php
+++ theme/gigasci/content.php
@@ -1,14 +1,7 @@
-<div class="jumbotron">
-	<h1 class="display-3">Hello, world!</h1>
-	<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-	<hr class="my-4">
-	<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-</div>
-
 <div class="blog-post">
 	<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 
-	<?php the_content(); ?>
+	<?php the_excerpt(); ?>
 
 </div><!-- /.blog-post -->
