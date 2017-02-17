0d0cfc75024976efb119c367c7194c1a93f78669
pli888 <peter@gigasciencejournal.com>
2017-02-17 15:00:07 +0800

    Add panels in sidebar

--- theme/gigasci/index.php
+++ theme/gigasci/index.php
@@ -98,7 +98,7 @@
                 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
             </div>
         </div>
-    </div>
+    </div><!-- /.blog-sidebar -->
 
     <div class="row top-buffer">
         <div class="col-sm-8 blog-main">
@@ -126,53 +126,41 @@
         <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
 			<div class="sidebar-module sidebar-module-inset">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Panel heading without title</div>
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Subscribe to GigaBlog</h3>
+                    </div>
                     <div class="panel-body">
-                        Panel content
+                        Enter your email
                     </div>
                 </div>
 
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h3 class="panel-title">Panel title</h3>
+                        <h3 class="panel-title">Popular Posts</h3>
                     </div>
                     <div class="panel-body">
-                        Panel content
+                        A post
                     </div>
                 </div>
 
-				<h4>Subscribe to GigaBlog</h4>
-				<p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-			</div>
-			<div class="sidebar-module">
-				<h4>Archives</h4>
-				<ol class="list-unstyled">
-					<li><a href="#">March 2014</a></li>
-					<li><a href="#">February 2014</a></li>
-					<li><a href="#">January 2014</a></li>
-					<li><a href="#">December 2013</a></li>
-					<li><a href="#">November 2013</a></li>
-					<li><a href="#">October 2013</a></li>
-					<li><a href="#">September 2013</a></li>
-					<li><a href="#">August 2013</a></li>
-					<li><a href="#">July 2013</a></li>
-					<li><a href="#">June 2013</a></li>
-					<li><a href="#">May 2013</a></li>
-					<li><a href="#">April 2013</a></li>
-				</ol>
-			</div>
-			<div class="sidebar-module">
-				<h4>Elsewhere</h4>
-				<ol class="list-unstyled">
-					<li><a href="#">GitHub</a></li>
-					<li><a href="#">Twitter</a></li>
-					<li><a href="#">Facebook</a></li>
-				</ol>
-			</div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Archive</h3>
+                    </div>
+                    <div class="panel-body">
+                        A post
+                    </div>
+                </div>
+
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Popular tags</h3>
+                    </div>
+                    <div class="panel-body">
+                        A post
+                    </div>
+                </div>
 		</div><!-- /.blog-sidebar -->
-
-
-
 	</div><!-- /.row -->
 
 </div><!-- /.container -->
