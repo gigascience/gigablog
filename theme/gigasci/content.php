4287bb5947ad3da3258801fa81c8463532adbf11
pli888 <peter@gigasciencejournal.com>
2017-02-20 17:23:36 +0800

    Separate date from blog post author name

--- theme/gigasci/content.php
+++ theme/gigasci/content.php
@@ -1,6 +1,7 @@
 <div class="blog-post">
-	<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+	<h3 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+    <p class="blog-post-meta"><?php the_date(); ?></p>
+    <a href="#"><?php the_author(); ?></a>
 
 	<?php the_excerpt(); ?>
 
@@ -8,5 +9,7 @@
 		<?php
 		printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( 						get_comments_number() ) ); ?>
     </a>
+
+    <hr>
 
 </div><!-- /.blog-post -->
