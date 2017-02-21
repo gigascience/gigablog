2192e521af8f36a210a2b418ad95dfb00e791eed
pli888 <peter@gigasciencejournal.com>
2017-02-21 15:47:22 +0800

    Update footer with links and logo images

--- theme/gigasci/footer.php
+++ theme/gigasci/footer.php
@@ -4,36 +4,30 @@
 		<hr>
 		<div class="col-sm-3">
 			<h4>About Us</h4>
-			<ul>
-				<li><a href="#">GigaScience</a></li>
-				<li><a href="#">GigaDB</a></li>
-				<li><a href="#">GigaGalaxy</a></li>
+			<ul class="list-unstyled list-group">
+				<li><a href="https://academic.oup.com/gigascience">GigaScience</a></li>
+				<li><a href="http://gigadb.org">GigaDB</a></li>
+				<li><a href="http://gigagalaxy.net">GigaGalaxy</a></li>
 			</ul>
 		</div>
 
 		<div class="col-sm-3">
 			<h4>Social Media</h4>
-			<ul>
-				<li><a href="#">Facebook</a></li>
-				<li><a href="#">Twitter</a></li>
-				<li><a href="#">Google+</a></li>
-				<li><a href="#">Sina Webo</a></li>
-				<li><a href="#">LinkedIn</a></li>
+			<ul class="list-unstyled list-group">
+				<li><a href="https://www.facebook.com/GigaScience/">Facebook</a></li>
+				<li><a href="https://twitter.com/GigaScience">Twitter</a></li>
+				<li><a href="https://plus.google.com/+GigaScienceJournal">Google+</a></li>
+				<li><a href="http://weibo.com/gigasciencejournal">Sina Webo</a></li>
+				<li><a href="https://www.linkedin.com/company/gigascience">LinkedIn</a></li>
 			</ul>
 		</div>
 
 		<div class="col-sm-3">
-			<h4>BGI</h4>
-			<p>
-				<a href="#">Back to top</a>
-			</p>
+			<img src="<?php bloginfo('template_directory');?>/inc/images/BGI_logo.png">
 		</div>
 
 		<div class="col-sm-3">
-			<h4>Oxford University Press</h4>
-			<p>
-				<a href="#">Back to top</a>
-			</p>
+            <img src="<?php bloginfo('template_directory');?>/inc/images/oup-logo-footer.png">
 		</div>
 
 	</div>
@@ -44,6 +38,8 @@
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
 </body>
 </html>
