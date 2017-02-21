507dbaea97e6172bf0562befa16391d946a08efb
pli888 <peter@gigasciencejournal.com>
2017-02-21 10:53:49 +0800

    Use light theme in twitter feed

--- theme/gigasci/sidebar.php
+++ theme/gigasci/sidebar.php
@@ -1,10 +1,22 @@
 <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
-	<div id="twitter_timeline" style="height:400px;">
-		<a class="twitter-timeline" data-dnt="true" height="400" href="https://twitter.com/gigascience" data-widget-id="385649284785266688">Tweets by @GigaScience</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-	</div>
-
-    <hr>
+    <div class="panel panel-default">
+        <div id="twitter_timeline" style="height:400px;">
+            <a class="twitter-timeline" data-theme="light"
+               data-dnt="true" height="400"
+               href="https://twitter.com/gigascience"
+               data-widget-id="385649284785266688">Tweets by
+                @GigaScience</a>
+            <script>!function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                    if (!d.getElementById(id)) {
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = p + "://platform.twitter.com/widgets.js";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }
+                }(document, "script", "twitter-wjs");</script>
+        </div>
+    </div>
 
 	<div class="sidebar-module">
 		<div class="panel panel-default">
