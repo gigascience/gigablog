30582517df705af5191d58ffccebb0256ab8299b
pli888 <peter@gigasciencejournal.com>
2017-02-23 17:29:28 +0800

    Move horizontal rule

--- theme/gigasci/content.php
+++ theme/gigasci/content.php
@@ -1,4 +1,7 @@
 <div class="blog-post">
+
+    <hr>
+
 	<h3 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
     <p class="blog-post-meta"><?php the_date(); ?></p>
     <a href="#"><?php the_author(); ?></a>
@@ -27,7 +30,5 @@
 		<?php
 		printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( 						get_comments_number() ) ); ?>
     </a>
-
-    <hr>
 
 </div><!-- /.blog-post -->
