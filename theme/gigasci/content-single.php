7e9f37d68d52769041e34bb75ea064c77cf69948
pli888 <peter@gigasciencejournal.com>
2017-02-27 11:08:04 +0800

    Add author profile on blog post page

--- theme/gigasci/content-single.php
+++ theme/gigasci/content-single.php
@@ -1,55 +1,50 @@
 <div class="row">
 
-    <div class="col-sm-9 blog-main">
-
-<div class="blog-post">
-	<h2 class="blog-post-title"><?php the_title(); ?></h2>
-	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
-	<?php the_content(); ?>
-</div><!-- /.blog-post -->
+    <div class="col-md-8">
+        <div class="blog-post">
+            <h2 class="blog-post-title"><?php the_title(); ?></h2>
+            <p class="blog-post-meta"><?php the_date(); ?></p>
+			<?php the_content(); ?>
+        </div><!-- /.blog-post -->
     </div>
 
-    <div class="col-sm-3 blog-sidebar">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">Author</h3>
-            </div>
-            <div class="panel-body">
-                <p>Stuff goes here</p>
-            </div>
-
+    <div class="col-md-3 col-md-offset-1">
+        <div class="panel-heading sidebar-author">
+			<?php echo get_avatar( get_the_author_meta( 'ID' ), 64 ); ?>
+            <h3 class="sidebar-content sidebar-author"><?php the_author(); ?></h3>
+        </div>
+        <div class="panel-body">
+            <p class="sidebar-content"><?php the_author_description(); if(!get_the_author_description()) _e('No description. Please update your profile.','lightword'); ?></p>
         </div>
 
+
         <div class="sidebar-module">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-
-                    <h3 class="panel-title "><span class="glyphicon glyphicon-envelope"></span> Subscribe to GigaBlog</h3>
-
-                </div>
-                <div class="panel-body">
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email">
-                </div>
+            <div class="panel-heading">
+                <h3 class="panel-title sidebar-title"><span
+                            class="glyphicon glyphicon-envelope"></span>
+                    Subscribe to GigaBlog</h3>
+            </div>
+            <div class="panel-body">
+                <input type="email" class="form-control"
+                       id="exampleInputEmail1"
+                       aria-describedby="emailHelp"
+                       placeholder="Enter your email">
             </div>
 
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Archive</h3>
-                </div>
-                <div class="panel-body">
-                    <ol class="list-unstyled">
-						<?php wp_get_archives( 'type=monthly&limit=6&show_post_count=true' ); ?>
-                    </ol>
-                </div>
+            <div class="panel-heading">
+                <h3 class="panel-title sidebar-title">Archive</h3>
+            </div>
+            <div class="panel-body">
+                <ol class="list-unstyled">
+					<?php wp_get_archives( 'format=custom&before=<p class="sidebar-content">&after=</p>&type=monthly&limit=6' ); ?>
+                </ol>
             </div>
 
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Popular tags</h3>
-                </div>
-                <div class="panel-body">
-					<?php the_tags( '<ul class="list-unstyled list-group"><li>', '</li><li>', '</li></ul>' ); ?>
-                </div>
+            <div class="panel-heading">
+                <h3 class="panel-title sidebar-title">Popular tags</h3>
+            </div>
+            <div class="panel-body">
+				<?php the_tags( '<ul class="list-unstyled list-group"><li class="sidebar-content">', '</li><li class="sidebar-content">', '</li></ul>' ); ?>
             </div>
         </div>
     </div><!-- /.blog-sidebar -->
