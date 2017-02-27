bbcbe3f420193cad68b1a447b38d8d0086b6d699
pli888 <peter@gigasciencejournal.com>
2017-02-27 11:12:02 +0800

    Reduce space between author name and author description in sidebar

--- theme/gigasci/content-single.php
+++ theme/gigasci/content-single.php
@@ -9,9 +9,9 @@
     </div>
 
     <div class="col-md-3 col-md-offset-1">
-        <div class="panel-heading sidebar-author">
+        <div class="sidebar-author">
 			<?php echo get_avatar( get_the_author_meta( 'ID' ), 64 ); ?>
-            <h3 class="sidebar-content sidebar-author"><?php the_author(); ?></h3>
+            <h3 class="sidebar-author"><?php the_author(); ?></h3>
         </div>
         <div class="panel-body">
             <p class="sidebar-content"><?php the_author_description(); if(!get_the_author_description()) _e('No description. Please update your profile.','lightword'); ?></p>
