de126a65a864fa1e07871a31f066d6a748f30dab
pli888 <peter@gigasciencejournal.com>
2017-02-28 12:06:53 +0800

    Add author template
    This is used to display information about an author and lists their
    blog posts.

--- theme/gigasci/content-single.php
+++ theme/gigasci/content-single.php
@@ -11,7 +11,7 @@
     <div class="col-md-3 col-md-offset-1">
         <div class="sidebar-author">
 			<?php echo get_avatar( get_the_author_meta( 'ID' ), 72 ); ?>
-            <h3 class="sidebar-content"><?php the_author(); ?></h3>
+            <h3 class="sidebar-content"><?php the_author_posts_link(); ?></h3>
         </div>
         <div class="panel-body">
             <p class="sidebar-content"><?php the_author_description(); if(!get_the_author_description()) _e('No description. Please update your profile.','lightword'); ?></p>
