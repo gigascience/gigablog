fac438608054e6f0f34db6e5468361e251d31248
pli888 <peter@gigasciencejournal.com>
2017-03-01 11:41:01 +0800

    Constrain comments layout to col-md-8

--- theme/gigasci/single.php
+++ theme/gigasci/single.php
@@ -5,14 +5,17 @@
 
 			<?php
 			if ( have_posts() ) : while ( have_posts() ) : the_post();
-				get_template_part( 'content-single', get_post_format() );
+				get_template_part( 'content-single', get_post_format() ); ?>
 
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+            <div class="col-md-8">
+            <?php
+            if ( comments_open() || get_comments_number() ) :
+                comments_template();
+            endif;
 
 			endwhile; endif;
 			?>
+            </div>
 
 		</div> <!-- /.col -->
 	</div> <!-- /.row -->
