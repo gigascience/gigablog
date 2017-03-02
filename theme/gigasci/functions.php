4322b470f892ba7a8a03accdd8a5fb33762a48ae
pli888 <peter@gigasciencejournal.com>
2017-03-02 10:30:39 +0800

    Customise display of comments
    This is done using a callback function called gigasci_comment in
    functions.php.

--- theme/gigasci/functions.php
+++ theme/gigasci/functions.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Dazzling functions and definitions
+ * gigasci functions and definitions
  *
- * @package dazzling
+ * @package gigasci
  */
 
 
@@ -85,4 +85,36 @@ function comment_placeholders( $fields )
 	);
 
 	return $fields;
+}
+
+/**
+ * Customise display of comments.
+ *
+ * @param  array $fields
+ * @return array
+ */
+function gigasci_comment($comment, $args, $depth)
+{
+	$GLOBALS['comment'] = $comment; ?>
+	<li class="comment-list" <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
+	<div id="comment-<?php comment_ID(); ?>">
+		<div class="comment-author vcard">
+			<?php printf(__('<cite class="fn">%s</cite>'), get_comment_author_link()) ?>
+		</div>
+		<?php if ($comment->comment_approved == '0') : ?>
+			<em><?php _e('Your comment is awaiting moderation.') ?></em>
+			<br/>
+		<?php endif; ?>
+
+		<div class="comment-meta commentmetadata comment-metadata"><a
+				href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)) ?>"><?php printf(__('%1$s'), get_comment_date()) ?></a><?php edit_comment_link(__('(Edit)'), '  ', '') ?>
+		</div>
+
+		<?php comment_text() ?>
+
+		<div class="reply">
+			<?php comment_reply_link(array_merge($args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+		</div>
+	</div>
+	<?php
 }
